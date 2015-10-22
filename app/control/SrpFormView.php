d915c6f3a2d43a32a89e955f6f31e5b334d12224
Anderson <anderson_matogrosso@hotmail.com>
2015-10-22 00:56:16 -0200

    Criação da Consulta SRP - Inclusão da consulta SRP no menu Consulta

---
+++ app/control/SrpFormView.php
@@ -0,0 +1,246 @@
+<?php
+
+use Adianti\Control\TAction;
+use Adianti\Control\TPage;
+use Adianti\Database\TTransaction;
+use Adianti\Registry\TSession;
+use Adianti\Widget\Container\THBox;
+use Adianti\Widget\Container\TTable;
+use Adianti\Widget\Container\TTableRow;
+use Adianti\Widget\Container\TVBox;
+use Adianti\Widget\Datagrid\TDataGrid;
+use Adianti\Widget\Datagrid\TDataGridAction;
+use Adianti\Widget\Datagrid\TDataGridColumn;
+use Adianti\Widget\Datagrid\TPageNavigation;
+use Adianti\Widget\Dialog\TMessage;
+use Adianti\Widget\Form\TButton;
+use Adianti\Widget\Form\TDate;
+use Adianti\Widget\Form\TEntry;
+use Adianti\Widget\Form\TForm;
+use Adianti\Widget\Form\TLabel;
+
+/*
+ * Copyright (C) 2015 Anderson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * Description of SrpFormView
+ *
+ * @author Anderson
+ */
+class SrpFormView extends TPage {
+
+    protected $form; //formulario
+    protected $datagrid;
+
+    function __construct() {
+        parent::__construct();
+
+        //cria o formulario
+        $this->form = new TForm('SrpFormView');
+        $this->form->class = 'tform';
+        //$this->form->style = 'max-width: 500px';
+
+        $table = new TTable;
+        $table->width = '100%';
+        $this->form->add($table);
+
+        $row = $table->addRow();
+        $row->class = 'tformtitle';
+        $row->addCell(new TLabel('Listagem de Itens da SRP'))
+                ->colspan = 2;
+
+        //cria os campos do formulário
+        $id = new TEntry('id');
+        $numeroSRP = new TEntry('numeroSRP');
+        $numeroIRP = new TEntry('numeroIRP');
+        $numeroProcesso = new TEntry('numeroProcesso');
+        $nome = new TEntry('nome');
+        $uasg = new TEntry('uasg');
+        $validade = new TDate('validade');
+        $natureza = new TEntry('natureza');
+
+        // define os tamanhos
+        $id->setSize(70);
+        $numeroSRP->setSize(90);
+        $numeroIRP->setSize(90);
+        $numeroProcesso->setSize(120);
+        $nome->setSize(350);
+        $uasg->setSize(70);
+        $validade->setSize(90);
+        $natureza->setSize(200);
+
+        //desabilitando os campos
+        $id->setEditable(false);
+        $numeroSRP->setEditable(false);
+        $numeroIRP->setEditable(false);
+        $numeroProcesso->setEditable(false);
+        $nome->setEditable(false);
+        $uasg->setEditable(false);
+        $validade->setEditable(false);
+        $natureza->setEditable(false);
+
+        // adiciona uma linha para cada campo no formulario
+        $table->addRowSet(new TLabel('id:'), $id);
+        $table->addRowSet(new TLabel('Nº SRP:'), $numeroSRP);
+        $table->addRowSet(new TLabel('Nº IRP:'), $numeroIRP);
+        $table->addRowSet(new TLabel('Proc. Orig.:'), $numeroProcesso);
+        $table->addRowSet(new TLabel('Nome:'), $nome);
+        $table->addRowSet(new TLabel('UASG:'), $uasg);
+        $table->addRowSet(new TLabel('Validade:'), $validade);
+        $table->addRowSet(new TLabel('Natureza:'), $natureza);
+
+        $this->form->setFields(array($id, $numeroSRP, $numeroIRP, $numeroProcesso,
+            $nome, $uasg, $validade, $natureza));
+
+
+        //criar a datagrid
+        $this->datagrid = new TDataGrid;
+        $this->datagrid->makeScrollable();
+        $this->datagrid->disableDefaultClick();
+        $this->datagrid->setHeight(300);
+
+        //criar as colunas da datagrid 
+        $GnumeroItem = new TDataGridColumn('numeroItem', 'Nº Item', 'left', 50);
+        $GdescricaoSumaria = new TDataGridColumn('descricaoSumaria', 'Descrição Sumária', 'left', 400);
+        $GquantidadeEstimada = new TDataGridColumn('quantidadeDisponivel', 'Qtd. Estimada', 'right', 100);
+        $GquantidadeDisponivel = new TDataGridColumn('estoqueDisponivel', 'Qtd. Disponível', 'right', 100);
+        $GunidadeMedida = new TDataGridColumn('unidadeMedida', 'Unidade', 'left', 50);
+        $GvalorUnitario = new TDataGridColumn('valorUnitario', 'Valor Unit.', 'right', 70);
+
+        // add the columns to the DataGrid
+        $this->datagrid->addColumn($GnumeroItem);
+        $this->datagrid->addColumn($GdescricaoSumaria);
+        $this->datagrid->addColumn($GquantidadeEstimada);
+        $this->datagrid->addColumn($GquantidadeDisponivel);
+        $this->datagrid->addColumn($GunidadeMedida);
+        $this->datagrid->addColumn($GvalorUnitario);
+
+
+        /*
+        $viewDetalhe = new TDataGridAction(array($this, 'onShowDetail'));
+        $viewDetalhe->setLabel('Detalhes');
+        $viewDetalhe->setImage('ico_view.png');
+        $viewDetalhe->setField('id');
+
+        $this->datagrid->addAction($viewDetalhe);
+         */
+
+        // cria o modelo no datagrid
+        $this->datagrid->createModel();
+
+        $back_button = new TButton('back');
+        $back_button->setAction(new TAction(array('SrpList', 'onReload')), 'Voltar');
+        $back_button->setImage('ico_back.png');
+        $this->form->addField($back_button);
+
+        $buttons_box = new THBox;
+        $buttons_box->add($back_button);
+
+        // add a row for the form action
+        $row = $table->addRow();
+        $row->class = 'tformaction'; // CSS class
+        $row->addCell($buttons_box)->colspan = 2;
+
+        // cria o container da pagina
+        $container = TVBox::pack($this->form, $this->datagrid);
+        parent::add($container);
+    }
+
+    /*
+    public function onShowDetail($param = NULL) {
+        $pos = $this->datagrid->getRowIndex('id', $param['key']);
+        
+        // get row by position
+        $current_row = $this->datagrid->getRow($pos);
+        $current_row->style = "background-color: #8D8BC8; color:white; text-shadow:none";
+        
+        // create a new row
+        $row = new TTableRow;
+        $row->style = "background-color: #E0DEF8";
+        //$row->addCell('123');
+
+        
+        //$this->datagrid
+        $cell = $row->addCell('teste' . $current_row->nome);
+        $cell->colspan = 6;
+        $cell->style='padding:10px;';
+        
+        // insert the new row
+        $this->datagrid->insert($pos +1, $row);
+    }*/
+    
+    function onReload($param = null)
+    {
+        $this->datagrid->clear();
+                
+        $key = $param['key'];
+        if (!isset($key)){
+            
+            $value = TSession::getValue('srp_form_view_key');
+            if (!isset($value))
+                return;
+            
+            $key = $value;
+        }
+        
+        try {
+            TTransaction::open('saciq');
+            $srp = new Srp($key);
+
+            if (!isset($srp)) {
+                TTransaction::close();
+                return;
+            }
+            $object = new stdClass();
+            $object->id = $srp->id;
+            $object->numeroSRP = $srp->numeroSRP;
+            $object->numeroIRP = $srp->numeroIRP;
+            $object->numeroProcesso = $srp->numeroProcesso;
+            $object->uasg = $srp->uasg;
+            $object->validade = TDate::date2br($srp->validade);
+            $object->nome = $srp->nome;
+            $object->natureza = $srp->natureza->descricao;
+            
+            foreach ($srp->getItems() as $item) {
+                $row = $this->datagrid->addItem($item);
+            }           
+            
+            TForm::sendData('SrpFormView', $object);
+
+            TTransaction::close();
+        } catch (Exception $e) {
+            new TMessage('error', $e->getMessage());
+            TTransaction::rollback();
+        }
+    }
+
+    public function onViewSrp($param = NULL) {
+        $key = $param['key'];
+        if (!isset($key))
+            return;
+        TSession::setValue('srp_form_view_key', $key);
+        
+        $this->onReload($param);
+    }
+    
+    function show(){
+        $this->onReload();
+        parent::show();
+    }
+
+}
---
+++ app/control/SrpList.class.php
@@ -0,0 +1,339 @@
+<?php
+
+use Adianti\Control\TAction;
+use Adianti\Control\TPage;
+use Adianti\Database\TCriteria;
+use Adianti\Database\TFilter;
+use Adianti\Database\TRepository;
+use Adianti\Database\TTransaction;
+use Adianti\Registry\TSession;
+use Adianti\Widget\Container\TTable;
+use Adianti\Widget\Container\TVBox;
+use Adianti\Widget\Datagrid\TDataGrid;
+use Adianti\Widget\Datagrid\TDataGridAction;
+use Adianti\Widget\Datagrid\TDataGridColumn;
+use Adianti\Widget\Datagrid\TPageNavigation;
+use Adianti\Widget\Dialog\TMessage;
+use Adianti\Widget\Form\TButton;
+use Adianti\Widget\Form\TDate;
+use Adianti\Widget\Form\TEntry;
+use Adianti\Widget\Form\TForm;
+use Adianti\Widget\Form\TLabel;
+
+/*
+ * Copyright (C) 2015 Anderson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+/**
+ * Description of SrpList
+ *
+ * @author Anderson
+ */
+class SrpList extends TPage{
+    private $form;     // registration form
+    private $datagrid; // listing
+    private $pageNavigation;
+    private $loaded;
+    
+    function __construct() {
+        parent::__construct();
+        
+        //criar o form
+        $this->form = new TForm('form_consulta_srp');
+        $this->form->class = 'tform';
+        
+        //cria a tabela
+        $table = new TTable;
+        $table->width = '100%';
+        $this->form->add($table);
+        
+        //adiciona uma linha na tabela para o titulo
+        $row = $table->addRow();
+        $row->class = 'tformtitle';
+        $row->addCell(new TLabel('Consulta SRP'))->colspan = 2;
+        
+        // cria os campos do formulario
+        $numeroSRP        = new TEntry('numeroSRP');
+        $numeroIRP        = new TEntry('numeroIRP');
+        $numeroProcesso   = new TEntry('numeroProcesso');
+        $uasg             = new TEntry('uasg');
+        $validade         = new TEntry('validade');
+        $nome             = new TEntry('nome');
+        
+        // define os tamanhos
+        $numeroSRP->setSize(70);
+        $numeroIRP->setSize(70);
+        $numeroProcesso->setSize(150);
+        $uasg->setSize(70);
+        $validade->setSize(100);
+        $nome->setSize(400);        
+        $validade->setMask('dd/mm/yyyy');
+        
+        // adiciona uma linha na tabela para cada campo
+        $table->addRowSet( new TLabel('Nº SRP:'), $numeroSRP );
+        $table->addRowSet( new TLabel('Nº IRP:'), $numeroIRP );
+        $table->addRowSet( new TLabel('Proc. Orig.:'), $numeroProcesso );
+        $table->addRowSet( new TLabel('Nome:'), $nome );
+        $table->addRowSet( new TLabel('UASG:'), $uasg );
+        $table->addRowSet( new TLabel('Validade:'), $validade );
+        
+        $this->form->setFields(array($numeroSRP,$numeroIRP,$numeroProcesso,$uasg,$validade,$nome));
+        
+        // manter o formulario preenchido durante navegação com os dados da sessao
+        $this->form->setData( TSession::getValue('Srp_filter_data') );
+        
+        //cria o botão de ação
+        $find_button = TButton::create('find', array($this, 'onSearch'), 'Consultar', 'ico_find.png');
+        
+        $this->form->addField($find_button);
+        
+        // adiciona uma linha para a acao do formulario
+        $row = $table->addRow();
+        $row->class = 'tformaction'; // CSS class
+        $row->addCell($find_button)->colspan = 2;
+        
+        // cria o datagrid
+        $this->datagrid = new TDataGrid;
+        $this->datagrid->setHeight(320);
+        
+
+        // cria as colunas do datagrid
+        $numeroSRP   = new TDataGridColumn('numeroSRP', 'Nº SRP', 'left', 70);
+        $numeroIRP   = new TDataGridColumn('numeroIRP', 'Nº IRP', 'left', 70);
+        $numeroProcesso   = new TDataGridColumn('numeroProcesso', 'Proc. Orig.', 'left', 150);
+        $uasg   = new TDataGridColumn('uasg', 'UASG', 'left', 50);
+        $validade   = new TDataGridColumn('validade', 'Validade', 'left', 100);
+        $nome   = new TDataGridColumn('nome', 'Nome', 'left', 300);
+        
+        $numeroSRP->setTransformer(array($this, 'rowFormat'));
+
+
+        // adiciona as colunas ao datagrid
+        $this->datagrid->addColumn($numeroSRP);
+        $this->datagrid->addColumn($numeroIRP);
+        $this->datagrid->addColumn($numeroProcesso);
+        $this->datagrid->addColumn($nome);
+        $this->datagrid->addColumn($uasg);
+        $this->datagrid->addColumn($validade);
+        
+
+        
+        // cria as ações do datagrid
+
+        /*
+        $action2 = new TDataGridAction(array($this, 'onDelete'));
+        $action2->setLabel(_t('Delete'));
+        $action2->setImage('ico_delete.png');
+        $action2->setField('id');*/
+        
+        // add the actions to the datagrid
+        //$this->datagrid->addAction($action1);
+        //$this->datagrid->addAction($action2);
+        
+        $srpViewAction = new TDataGridAction(array('SrpFormView','onViewSrp'));
+        $srpViewAction->setLabel('Itens');
+        $srpViewAction->setImage('fa:th-list');
+        $srpViewAction->setField('id');
+        
+        $this->datagrid->addAction($srpViewAction);
+        
+        // cria o modelo no datagrid
+        $this->datagrid->createModel();
+        
+        // cria o navegador de pagina
+        $this->pageNavigation = new TPageNavigation;
+        $this->pageNavigation->setAction(new TAction(array($this, 'onReload')));
+        $this->pageNavigation->setWidth($this->datagrid->getWidth());
+        
+        // cria o container da pagina
+        $container = TVBox::pack( $this->form, $this->datagrid, $this->pageNavigation);
+        parent::add($container);
+    }
+    
+    function onSearch(){
+        // pega os dados do formulario
+        $data = $this->form->getData();
+        
+        // limpa os filtros da sessao
+        TSession::setValue('SrpList_filter_numeroSRP',   NULL);
+        TSession::setValue('SrpList_filter_numeroIRP',   NULL);
+        TSession::setValue('SrpList_filter_numeroProcesso',   NULL);
+        TSession::setValue('SrpList_filter_uasg',   NULL);
+        TSession::setValue('SrpList_filter_validade',   NULL);
+        TSession::setValue('SrpList_filter_nome',   NULL);
+
+        if (isset($data->numeroSRP) AND ($data->numeroSRP)) {
+            $filter = new TFilter('numeroSRP', 'like', "%{$data->numeroSRP}%"); // cria o filtro
+            TSession::setValue('SrpList_filter_numeroSRP',   $filter); // armazena o filtro na sessao
+        }
+
+
+        if (isset($data->numeroIRP) AND ($data->numeroIRP)) {
+            $filter = new TFilter('numeroIRP', 'like', "%{$data->numeroIRP}%"); // cria o filtro
+            TSession::setValue('SrpList_filter_numeroIRP',   $filter); // armazena o filtro na sessao
+        }
+
+
+        if (isset($data->numeroProcesso) AND ($data->numeroProcesso)) {
+            $filter = new TFilter('numeroProcesso', 'like', "%{$data->numeroProcesso}%"); // cria o filtro
+            TSession::setValue('SrpList_filter_numeroProcesso',   $filter); // armazena o filtro na sessao
+        }
+
+
+        if (isset($data->uasg) AND ($data->uasg)) {
+            $filter = new TFilter('uasg', 'like', "%{$data->uasg}%"); // cria o filtro
+            TSession::setValue('SrpList_filter_uasg',   $filter); // armazena o filtro na sessao
+        }
+
+
+        if (isset($data->validade) AND ($data->validade)) {
+            $filter = new TFilter('validade', '=',TDate::date2us($data->validade)); // cria o filtro
+            TSession::setValue('SrpList_filter_validade',   $filter); // armazena o filtro na sessao
+        }
+
+
+        if (isset($data->nome) AND ($data->nome)) {
+            $filter = new TFilter('nome', 'like', "%{$data->nome}%"); // cria o filtro
+            TSession::setValue('SrpList_filter_nome',   $filter); // armazena o filtro na sessao
+        }
+
+        
+        // fill the form with data again
+        $this->form->setData($data);
+        
+        // keep the search data in the session
+        TSession::setValue('Srp_filter_data', $data);
+        
+        $param=array();
+        $param['offset']    =0;
+        $param['first_page']=1;
+        $this->onReload($param);
+    }
+    
+    function onReload($param = NULL){
+        try
+        {
+            // abre uma transação com o banco 'saciq'
+            TTransaction::open('saciq');
+            TTransaction::setLogger(new \Adianti\Log\TLoggerTXT("c:\\array\\LOG".date("Ymd-His").".txt"));
+            // cria um repository para Srp
+            $repository = new TRepository('Srp');
+            $limit = 10;
+            // cria um criteria
+            $criteria = new TCriteria;
+            
+            // ordem default
+            if (empty($param['order']))
+            {
+                $param['order'] = 'id';
+                $param['direction'] = 'asc';
+            }
+            $criteria->setProperties($param); // ordem, offset
+            $criteria->setProperty('limit', $limit);
+            
+
+            if (TSession::getValue('SrpList_filter_numeroSRP')) {
+                $criteria->add(TSession::getValue('SrpList_filter_numeroSRP')); // add the session filter
+            }
+
+
+            if (TSession::getValue('SrpList_filter_numeroIRP')) {
+                $criteria->add(TSession::getValue('SrpList_filter_numeroIRP')); // add the session filter
+            }
+
+
+            if (TSession::getValue('SrpList_filter_numeroProcesso')) {
+                $criteria->add(TSession::getValue('SrpList_filter_numeroProcesso')); // add the session filter
+            }
+
+
+            if (TSession::getValue('SrpList_filter_uasg')) {
+                $criteria->add(TSession::getValue('SrpList_filter_uasg')); // add the session filter
+            }
+
+
+            if (TSession::getValue('SrpList_filter_validade')) {
+                $criteria->add(TSession::getValue('SrpList_filter_validade')); // add the session filter
+            }
+
+
+            if (TSession::getValue('SrpList_filter_nome')) {
+                $criteria->add(TSession::getValue('SrpList_filter_nome')); // add the session filter
+            }
+
+            
+            // carrega os objetos de acordo com os filtros
+            $objects = $repository->load($criteria, FALSE);
+            
+            $this->datagrid->clear();
+            if ($objects)
+            {
+                // iterar a coleção de active records
+                foreach ($objects as $object)
+                {
+                    //muda a data para o formato brasileiro (DD/MM/YYYY)
+                    $object->validade = TDate::date2br($object->validade);
+                    //adiciona o objeto no datagrid
+                    $this->datagrid->addItem($object);
+                }
+            }
+            
+            // reseta o criteria (filtro) para contagem de registros
+            $criteria->resetProperties();
+            $count= $repository->count($criteria);
+            
+            $this->pageNavigation->setCount($count); // contagem de registro
+            $this->pageNavigation->setProperties($param); // ordem, pagina
+            $this->pageNavigation->setLimit($limit); // limite
+            
+            // fecha a transação
+            TTransaction::close();
+            $this->loaded = true;
+        }
+        catch (Exception $e) // Em caso de erro, gera uma exceção
+        {
+            // mostra mensagem de erro da exceção
+            new TMessage('error', '<b>Error</b> ' . $e->getMessage());
+            
+            // rolback no na transação
+            TTransaction::rollback();
+        }
+    }
+    
+    public function rowFormat($id, $object, $row) {
+        
+        if ($object->estaVencida()) {
+            $row->style = "background: #FFDADE";
+        }
+        return $id;
+    }
+    
+    /**
+     * method show()
+     * Exibe a pagina
+     */
+    function show()
+    {
+        // checa se o datagrid ja foi preenchido
+        if (!$this->loaded AND (!isset($_GET['method']) OR $_GET['method'] !== 'onReload') )
+        {
+            $this->onReload( func_get_arg(0) );
+        }
+        parent::show();
+    }
+
+}
